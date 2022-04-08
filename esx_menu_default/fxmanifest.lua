664f30fd30689e8b5869d41f3e4bee2c529a4a96
Javi.Gnlz <98654716+JaviGnlzzz@users.noreply.github.com>
2022-04-08 09:39:18 +0200

    Add files via upload

---
+++ esx_menu_default/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'TusMuertos.#4903'
+
+description 'Menu Default rediseñado'
+
+files {
+	'**/**/**/*.*'
+}
+
+ui_page {
+	'html/ui.html'
+}
+
+client_scripts {
+	'client/main.lua'
+}
